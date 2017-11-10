0f1064fa448cd66f030bb3a919ed0b549befb9e7
manirox <32426583+manirox@users.noreply.github.com>
2017-11-10 16:43:38 -0600

    Update CodingAssigenmentonePart3.R

--- CodingAssigenmentonePart3.R
+++ CodingAssigenmentonePart3.R
@@ -1,5 +1,5 @@
 rm(list=ls())
-pollutantmean <- function(specdata,x)
+pollutantmean <- function(specdata,corrval)
 {
   filenames <- list.files(specdata, pattern="*.csv", full.names=TRUE)
   print(filenames[1:2])
@@ -10,6 +10,7 @@ pollutantmean <- function(specdata,x)
   
   temprbind = NULL
   #temprbind  =0
+  corrr = numeric()
   for(i in 1:2)
   {
     
@@ -17,22 +18,43 @@ pollutantmean <- function(specdata,x)
     
     
   }
+  
  OmmitingNas <- na.omit(temprbind)
- GettingSubset <- OmmitingNas[1:3,]
- print(GettingSubset)
+ #print(temprbind)
+ print(OmmitingNas)
+ GettingSubset <- OmmitingNas[1:corrval,]
+
+ print((GettingSubset))
+ sulphate <- GettingSubset$sulfate
+ nitrate <- GettingSubset$nitrate
  
- for(i in 1:length(GettingSubset))
+ for(i in 1:nrow(GettingSubset))
  {
- data.frame(ResToSupply) <- GettingSubset[,i]
+ #data.frame(ResToSupply) <- GettingSubset[,i]
  #print(class(ResToSupply[i]))
  #print(ResToSupply)
- print(i)
- print((ResToSupply[i]))
- #CorResult <- cor(ResToSupply$sulfate,ResToSupply$nitrate)
- #print(i)
- #print(CorResult)
+ print("--sulphate--")
+ print(sulphate[i])
+ print("--nitrate--")
+ print((nitrate[i]))
+ corrr = c(corrr, cor(sulphate[i], nitrate[i]))
+ print("the correlation of the result")
+ #cor(sulphate[i], nitrate[i])
+ print(cor(sulphate[i], nitrate[i]))
+ #CorResult <- cor(sulphate[i],nitrate[i])
+
+ #print(corrr)
  }
+ print(corrr)
   
 }
+cor(c(6,4),c(7,5))
+x <- mtcars[1:3]
+y <- mtcars[4:6]
+cor(x, y)
 
-pollutantmean("/Users/manishreddybendhi/Desktop/Fun/RProgrammingCoursera/specdata",1:300)
+
+#pollutantmean("/Users/manishreddybendhi/Desktop/Fun/RProgrammingCoursera/specdata",1:300)
+pollutantmean("C://Users//Mreddy//Desktop//MeshineLearning//DataScience//CourseraAssigenment-1//rprog%2Fdata%2Fspecdata//specdata",100)
+
+
